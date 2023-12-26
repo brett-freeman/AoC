67f5f0c5faeb8144381c121bf36d867cf503974e
= <=>
2023-12-26 15:37:00 -0500

    Day One init

---
+++ 2023/d1p1.py
@@ -0,0 +1,12 @@
+fd = open('sample_input', 'r')
+lines = fd.read().splitlines()
+ans = 0
+
+for line in lines:
+    digits = []
+    for c in line:
+        if c.isdigit():
+            digits.append(c)
+    ans += int(digits[0] + digits[-1])
+
+print(ans)
