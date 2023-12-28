058f0d4617582e4985dc5d97ec0977a03150acc9
= <=>
2023-12-27 19:26:04 -0500

    Day Three very early attempt.

--- 2023/d3p1.py
+++ 2023/d3p1.py
@@ -1,14 +1,23 @@
 fd = open('sample_input', 'r')
 lines = fd.read().splitlines()
 
-symbols = [
+partnums = []
+
+match_symbols = {
     '*',
-    '#',
+    '+',
     '$',
-    '+'
-]
+    '#'
+}
+
+numbers = {}
+symbols = {}
 
 for i, line in enumerate(lines):
     for j, c in enumerate(line):
-        if c.isdigit():
-            print(lines[i-1], lines[i+1])
+        if c == '.':
+            pass
+        elif c.isdigit():
+            numbers[(i, j)] = int(c)
+        else:
+            symbols[(i, j)] = c
